bde612e8e257d96390c6cbedf09c6014ff798db7
谢小军 <xxj@xiexiaojundeMacBook-Air.local>
2019-09-20 14:29:37 +0800

    new translate to english

--- fractional/fractional.go
+++ fractional/fractional.go
@@ -1,6 +1,3 @@
-/*
-Package fractional 分数运算相关
-*/
 package fractional
 
 import (
@@ -9,14 +6,16 @@ import (
 	"github.com/xxjwxc/public/mymath"
 )
 
-//FAL 分数
+// FAL Fractional operation correlation
 type FAL struct {
-	Nume int64 //numerator 分子
-	Deno int64 //denominator 分母 (一定不为0)
+	// Nume numerator
+	Nume int64 // 分子
+	// denominator (must not be zero)
+	Deno int64 // 分母 (一定不为0)
 }
 
-//Model Create a score (molecular, denominator) with a denominator default of 1 创建一个分数(分子，分母)，分母默认为1:
-func Model(nd ...int64) *FAL {
+// Model Create a score (molecular, denominator) with a denominator default of 1
+func Model(nd ...int64) *FAL { // 创建一个分数(分子，分母)，分母默认为1
 	var f FAL
 	if len(nd) == 1 {
 		f.Nume = nd[0]
@@ -26,20 +25,20 @@ func Model(nd ...int64) *FAL {
 		f.Deno = nd[1]
 	}
 
-	if f.Deno == 0 { //分母为0
+	if f.Deno == 0 { // denominator is 0 .分母为0
 		panic(fmt.Sprintf("fractional init error. if denominator can't zero."))
 	}
 
 	return &f
 }
 
-//阔张
+// Broadsheet  .阔张
 func (s *FAL) broad(lcm int64) {
 	s.Nume = s.Nume * (lcm / s.Deno)
 	s.Deno = lcm
 }
 
-//压缩 整理
+// Compression Finishing .压缩 整理
 func (s *FAL) offset() {
 	lcm := mymath.Gcd(s.Nume, s.Deno)
 
@@ -47,9 +46,9 @@ func (s *FAL) offset() {
 	s.Deno /= lcm
 }
 
-//Add 分数加法
+// Add Fraction addition
 func (s *FAL) Add(f *FAL) *FAL {
-	//获取最小公倍数
+	// Getting the Minimum Common Multiplier 获取最小公倍数
 	lcm := mymath.Lcm(f.Deno, s.Deno)
 	s.broad(lcm)
 	f.broad(lcm)
@@ -59,9 +58,9 @@ func (s *FAL) Add(f *FAL) *FAL {
 	return s
 }
 
-//Sub 分数减法
+// Sub fraction subtraction
 func (s *FAL) Sub(f *FAL) *FAL {
-	//获取最小公倍数
+	// Getting the Minimum Common Multiplier 获取最小公倍数
 	lcm := mymath.Lcm(s.Deno, f.Deno)
 	s.broad(lcm)
 	f.broad(lcm)
@@ -71,7 +70,7 @@ func (s *FAL) Sub(f *FAL) *FAL {
 	return s
 }
 
-//Mul 乘法
+// Mul multiplication
 func (s *FAL) Mul(f *FAL) *FAL {
 	s.Deno *= f.Deno
 	s.Nume *= f.Nume
@@ -79,15 +78,15 @@ func (s *FAL) Mul(f *FAL) *FAL {
 	return s
 }
 
-//Div 乘法
-func (s *FAL) Div(f *FAL) *FAL {
+// Div division
+func (s *FAL) Div(f *FAL) *FAL { // 除法
 	tmp := Model(f.Deno, f.Nume)
 	s.Mul(tmp)
 	s.offset()
 	return s
 }
 
-//Verdict 计算结果
-func (s *FAL) Verdict() float64 {
+// Verdict Calculation results
+func (s *FAL) Verdict() float64 { // 计算结果
 	return float64(s.Nume) / float64(s.Deno)
 }
